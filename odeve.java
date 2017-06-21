aba9ace9f045b25a9e3d99bb5be63c014046cf59
RANJITHKHAN <ranjithtech4@gmail.com>
2017-06-21 14:56:53 +0530

    Create odeve.java

---
+++ odeve.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class odeve
+{
+    public static void main(String args[])
+    {
+        Scanner a=new Scanner(System.in);
+        int n=a.nextInt();
+        System.out.println(n);
+        if(n%2==0)
+        {
+        System.out.println("even");
+        }
+        if(n%2!=0)
+        {
+        System.out.println("odd");
+        }
+        
+    }
+}
